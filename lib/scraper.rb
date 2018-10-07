b69148d7bdaf0d58b8edaac7d83ccbcbb8694cb1
Dan Hintz <dhintz@live.com>
2018-10-07 19:53:44 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
     site = Nokogiri::HTML(open(index_url))
     students = []
     site.css(".student-card").each do |student|
-      student.css(".card-text-container")
+      student.css(".card-text-container.")
     
   end
 
